315809cf0a5fda63c4cd7c45286632b29fdb990d
Elizabeth Brooks <merimaat1@gmail.com>
2022-10-11 17:38:44 -0400

    Updated oligo scripts

--- largeOligoRMA.R
+++ largeOligoRMA.R
@@ -12,19 +12,25 @@ workingDir = args[1];
 #workingDir="/scratch365/ebrooks5/GBCF_bioinformatics_DxTerity/GSE8888n_4_5_6"
 setwd(workingDir)
 
+# install packages
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-
 BiocManager::install("oligo")
+install.packages("ff")
 
+# load libraries
 library(oligo)
 library(ff)
 ldPath()
 
+# target CEL files
 celFiles <- list.celfiles(workingDir, full.names=TRUE)
 
+# read CEL files
 rawData <- read.celfiles(celFiles)
 
+# RMA normalization
 rmaRes <- rma(rawData)
 
+# view top 10 DE genes
 exprs(rmaRes)[1:10,]
