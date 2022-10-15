31ed388e7d786e1b038dec51ccba2c5d717e9afe
Elizabeth Brooks <merimaat1@gmail.com>
2022-10-14 20:15:59 -0400

    Added subseting to scripts

--- largeOligoRMA_driver.sh
+++ largeOligoRMA_driver.sh
@@ -3,7 +3,7 @@
 #$ -m abe
 #$ -r n
 #$ -N largeOligoRMA_jobOutput
-#$ -pe smp 16
+#$ -pe smp 8
 
 # usage: bash largeOligoRMA_driver.sh geoDir
 # usage ex: bash largeOligoRMA_driver.sh GSE8888n_4_5_6
@@ -11,6 +11,7 @@
 # usage ex: bash largeOligoRMA_driver.sh GSE88885
 # usage ex: bash largeOligoRMA_driver.sh GSE88886
 
+## note (full set)
 # Script to run RMA normalization with the oligo R package using a minimum of 16 cores (171GB)
 # the raw data is 82GB total, and a vector is created during normalization that is about 162GB
 # since each core has 10.7GB RAM available, slightly more than 15 cores are needed for the analysis
