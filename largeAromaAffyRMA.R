8c530908f44b701ae160eadd4bc0f2da73a3fc14
Elizabeth Brooks <merimaat1@gmail.com>
2022-10-16 23:38:31 -0400

    Added scripts for aroma.affy package

---
+++ largeAromaAffyRMA.R
@@ -0,0 +1,85 @@
+#!/usr/bin/env Rscript
+
+# script to RMA normalize newer affymetrix array data
+# usage: Rscript largeOligoRMA.R workingDir chipType celSet
+# usage ex: Rscript largeOligoRMA.R /scratch365/ebrooks5/GBCF_bioinformatics_DxTerity/rma_aromaAffy HTA-2_0 GSE8888n_4_5_6_CEL
+
+# Windows systems
+#Sys.getenv('R_MAX_VSIZE')
+#Sys.setenv('R_MAX_VSIZE'=171GB)
+
+# retrieve input file name of gene counts
+args = commandArgs(trailingOnly=TRUE)
+
+#Set working directory
+workingDir = args[1]
+#workingDir="/scratch365/ebrooks5/GBCF_bioinformatics_DxTerity/rma_aromaAffy"
+setwd(workingDir)
+
+# install packages
+#if (!require("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+#BiocManager::install("aroma.affymetrix")
+
+# load libraries
+library(aroma.affymetrix)
+
+# retrieve verbose info
+verbose <- Arguments$getVerbose(-8, timestamp=TRUE)
+
+# retrieve annotation data
+chipType <- args[2]
+#chipType <- "HTA-2_0"
+cdf <- AffymetrixCdfFile$byChipType(chipType, tags=c("r1", "gene"))
+print(cdf)
+
+# define CEL set
+celSet <- args[3]
+#celSet <- "GSE8888n_4_5_6_CEL"
+cs <- AffymetrixCelSet$byName("tissues", cdf=cdf)
+print(cs)
+
+# background adjustment and normalization
+bc <- RmaBackgroundCorrection(cs)
+csBC <- process(bc,verbose=verbose)
+
+# setup a quantile normalization method
+qn <- QuantileNormalization(csBC, typesToUpdate="pm")
+print(qn)
+
+# run quantile normalization
+csN <- process(qn, verbose=verbose)
+
+# fit the RMA probe level model (PLM)
+plm <- RmaPlm(csN)
+print(plm)
+
+# examine NUSE and RLE plots
+qam <- QualityAssessmentModel(plm)
+# NUSE
+png(file="plmFit_NUSE_RMA.png")
+plotNuse(qam)
+dev.off()
+# RLE
+png(file="plmFit_RLE_RMA.png")
+plotRle(qam)
+dev.off()
+
+# extract the estimates
+ces <- getChipEffectSet(plm)
+gExprs <- extractDataFrame(ces, units=1:3, addNames=TRUE)
+
+# save the normalized linear expression data to a RData file
+save(gExprs, file="normalizedLinear_RMA.RData")
+
+# write normalized linear expression to txt file
+capture.output(gExprs, file="normalizedLinear_RMA.txt")
+
+# log2 expression
+gExprs_log2 <- log2(gExprs)
+
+# save the normalized log2 expression data to a RData file
+save(exprsData, file="normalizedLog2_RMA.RData")
+
+# write normalized log2 expression to txt file
+capture.output(exprsData, file="normalizedLog2_RMA.txt")
--- largeAromaAffyRMA_driver.sh
+++ largeAromaAffyRMA_driver.sh
@@ -3,13 +3,9 @@
 #$ -m abe
 #$ -r n
 #$ -N largeOligoRMA_jobOutput
-#$ -pe smp 8
 
-# usage: qsub largeOligoRMA_driver.sh geoDir
-# usage ex: qsub largeOligoRMA_driver.sh GSE8888n_4_5_6
-# usage ex: qsub largeOligoRMA_driver.sh GSE88884
-# usage ex: qsub largeOligoRMA_driver.sh GSE88885
-# usage ex: qsub largeOligoRMA_driver.sh GSE88886
+# usage: qsub largeOligoRMA_driver.sh inputDir chipType celSet
+# usage ex: qsub largeOligoRMA_driver.sh /scratch365/ebrooks5/GBCF_bioinformatics_DxTerity/rma_aromaAffy HTA-2_0 GSE8888n_4_5_6_CEL
 
 ## note (full set)
 # Script to run RMA normalization with the oligo R package using a minimum of 16 cores (171GB)
@@ -17,10 +13,16 @@
 # since each core has 10.7GB RAM available, slightly more than 15 cores are needed for the analysis
 
 # load the R v4 module
-module load R/4.2.1
+module load R/3.6.2
 
-# set input file
-inputFile="/scratch365/ebrooks5/GBCF_bioinformatics_DxTerity/"$1"_CEL"
+# set inputs directory
+inputDir="$1"
+
+# set chip type
+chipType="$2"
+
+# set CEL set
+celSet="$3"
 
 # normalize all CEL files
-Rscript largeOligoRMA.R $inputFile
+Rscript largeAromaAffy.R $inputDir $chipType $celSet
