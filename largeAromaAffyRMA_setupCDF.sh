831af93ab7fcc975d4e006afecc96a869073b1b0
Elizabeth Brooks <merimaat1@gmail.com>
2022-10-18 01:50:08 -0400

    Fixed typos in CDF conversion job scripts

--- largeAromaAffyRMA_setupCDF.sh
+++ largeAromaAffyRMA_setupCDF.sh
@@ -4,9 +4,9 @@
 #$ -r n
 #$ -N setupAromaAffy_CDF_jobOutput
 
-# usage: qsub largeAromaAffyRMA_setupCDF.sh workingDir cdfPath
-# usage ex: qsub largeAromaAffyRMA_setupCDF.sh /afs/crc.nd.edu/group/genomics/PLAGUIES /scratch365/ebrooks5/GBCF_bioinformatics_DxTerity/HTA-2_0.r1.gene.cdf
-# usage ex: qsub largeAromaAffyRMA_setupCDF.sh /afs/crc.nd.edu/group/genomics/SCARIF /scratch365/ebrooks5/GBCF_bioinformatics_DxTerity/HTA-2_0.r1.gene.cdf
+# usage: qsub largeAromaAffyRMA_setupCDF.sh workingDir cdfPath outputName
+# usage ex: qsub largeAromaAffyRMA_setupCDF.sh /afs/crc.nd.edu/group/genomics/PLAGUIES /scratch365/ebrooks5/GBCF_bioinformatics_DxTerity/HTA-2_0.r1.gene.cdf HTA-2_0,r1,gene.cdf
+# usage ex: qsub largeAromaAffyRMA_setupCDF.sh /afs/crc.nd.edu/group/genomics/SCARIF /scratch365/ebrooks5/GBCF_bioinformatics_DxTerity/HTA-2_0.r1.gene.cdf HTA-2_0,r1,gene.cdf
 
 # load the R v4 module
 module load R/3.6.2
@@ -17,10 +17,13 @@ workingDir="$1"
 # set input CDF path
 cdfPath="$2"
 
+# set output name
+outputName="$3"
+
 # make sure to make necessary directories
 mkdir $workingDir"/GBCF_bioinformatics_DxTerity/rma_aromaAffy/annotationData"
 mkdir $workingDir"/GBCF_bioinformatics_DxTerity/rma_aromaAffy/annotationData/chipTypes"
 mkdir $workingDir"/GBCF_bioinformatics_DxTerity/rma_aromaAffy/annotationData/chipTypes/HTA-2_0/"
 
 # convert CDF file
-Rscript convertCDF_affxparser.R $cdfPath
+Rscript convertCDF_affxparser.R $workingDir"/GBCF_bioinformatics_DxTerity/rma_aromaAffy/annotationData/chipTypes/HTA-2_0/" $cdfPath $outputName
