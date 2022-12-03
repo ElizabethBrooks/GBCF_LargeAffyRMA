696957271c82831f09ff1341547f937ee6eeb8bb
Elizabeth Brooks <merimaat1@gmail.com>
2022-12-02 20:09:57 -0500

    Re-organized scripts

--- logTransformNormalizedCounts_driver.sh
+++ logTransformNormalizedCounts_driver.sh
@@ -17,26 +17,22 @@ celSet="GSE8888n_4_5_6_CEL"
 # set the number of columns
 numCols=3152
 
-# move to the data directory
-cd $workingDir
-
 # loop over each column for each sample
 # the first 6 columns are summary data
 for i in $(seq 7 $numCols); do
 	# retrieve the sample column data
-	cut -d "," -f $i normalizedLinear_RMA_$celSet.csv > normalizedLinear_RMA_$celSet_col$i.csv
+	cut -d "," -f $i $workingDir/normalizedLinear_RMA_$celSet.csv > $workingDir/normalizedLinear_RMA_$celSet_col$i.csv
 	# perform log2 transformation of the column
 	Rscript logTransformNormalizedCounts.r $workingDir $celSet col$i
 done
 
 # add summary data to the transformed data file
-cut -d , -f 1-6 normalizedLinear_RMA_$celSet.csv > normalizedLog_RMA_$celSet.csv
+cut -d , -f 1-6 $workingDir/normalizedLinear_RMA_$celSet.csv > $workingDir/normalizedLog_RMA_$celSet.csv
 
 # loop over each transformed column file for each sample and paste into one file
 for i in $(seq 7 $numCols); do
-	paste -d , normalizedLog_RMA_$celSet.csv normalizedLog_RMA_$celSet_col$i.csv
+	paste -d , $workingDir/normalizedLog_RMA_$celSet.csv $workingDir/normalizedLog_RMA_$celSet_col$i.csv
+	# clean up
+	rm $workingDir/normalizedLinear_RMA_$celSet_col$i.csv
+	rm $workingDir/normalizedLog_RMA_$celSet_col$i.csv
 done
-
-# clean up
-rm normalizedLinear_RMA_$celSet_col*.csv
-rm normalizedLog_RMA_$celSet_col*.csv
